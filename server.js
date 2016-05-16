950c51942f57f400bc85be59a33108534e3e5e13
vaibhav9312 <vaish0115@gmail.com>
2016-05-17 05:16:05 +0530

    klksls

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
+console.log("whatsup");
 var io = require('socket.io');
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
